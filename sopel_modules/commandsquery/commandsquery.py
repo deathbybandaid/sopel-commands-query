8203352bab4f94a1777dff195c1f6a049d9c5f53
deathbybandaid <sam@deathbybandaid.net>
2019-03-28 10:58:56 -0400

    replace long code with spicemanip import

--- sopel_modules/commandsquery/commandsquery.py
+++ sopel_modules/commandsquery/commandsquery.py
@@ -9,6 +9,8 @@ import os
 from difflib import SequenceMatcher
 from operator import itemgetter
 
+import spicemanip
+
 
 def configure(config):
     pass
@@ -116,7 +118,7 @@ def setup(bot):
 
             maincom = dict_from_file["validcoms"][0]
             if len(dict_from_file["validcoms"]) > 1:
-                comaliases = spicemanip(bot, dict_from_file["validcoms"], '2+', 'list')
+                comaliases = spicemanip.main(bot, dict_from_file["validcoms"], '2+', 'list')
             else:
                 comaliases = []
 
@@ -135,10 +137,10 @@ def query_detection(bot, trigger):
             if com not in commands_list.keys():
                 commands_list[com] = bot.memory['commandslist'][commandstype][com]
 
-    triggerargsarray = spicemanip(bot, trigger, 'create')
+    triggerargsarray = spicemanip.main(bot, trigger, 'create')
 
     # command issued, check if valid
-    querycommand = spicemanip(bot, triggerargsarray, 1).lower()[1:]
+    querycommand = spicemanip.main(bot, triggerargsarray, 1).lower()[1:]
     if len(querycommand) == 1:
         commandlist = []
         for command in commands_list.keys():
@@ -147,7 +149,7 @@ def query_detection(bot, trigger):
         if commandlist == []:
             return bot.notice("No commands match " + str(querycommand) + ".", trigger.nick)
         else:
-            return bot.notice("The following commands match " + str(querycommand) + ": " + spicemanip(bot, commandlist, 'andlist') + ".", trigger.nick)
+            return bot.notice("The following commands match " + str(querycommand) + ": " + spicemanip.main(bot, commandlist, 'andlist') + ".", trigger.nick)
 
     elif querycommand.endswith(tuple(["+"])):
         querycommand = querycommand[:-1]
@@ -157,7 +159,7 @@ def query_detection(bot, trigger):
         if "aliasfor" in commands_list[querycommand].keys():
             realcom = commands_list[querycommand]["aliasfor"]
         validcomlist = commands_list[realcom]["validcoms"]
-        return bot.notice("The following commands match " + str(querycommand) + ": " + spicemanip(bot, validcomlist, 'andlist') + ".", trigger.nick)
+        return bot.notice("The following commands match " + str(querycommand) + ": " + spicemanip.main(bot, validcomlist, 'andlist') + ".", trigger.nick)
 
     elif querycommand.endswith(tuple(['?'])):
         querycommand = querycommand[:-1]
@@ -167,13 +169,13 @@ def query_detection(bot, trigger):
             sim_com.append(com)
             sim_num.append(similarlevel)
         sim_num, sim_com = (list(x) for x in zip(*sorted(zip(sim_num, sim_com), key=itemgetter(0))))
-        closestmatch = spicemanip(bot, sim_com, 'reverse', "list")
+        closestmatch = spicemanip.main(bot, sim_com, 'reverse', "list")
         listnumb, relist = 1, []
         for item in closestmatch:
             if listnumb <= 10:
                 relist.append(str(item))
             listnumb += 1
-        return bot.notice("The following commands may match " + str(querycommand) + ": " + spicemanip(bot, relist, 'andlist') + ".", trigger.nick)
+        return bot.notice("The following commands may match " + str(querycommand) + ": " + spicemanip.main(bot, relist, 'andlist') + ".", trigger.nick)
 
     elif querycommand in commands_list.keys():
         return bot.notice("The following commands match " + str(querycommand) + ": " + str(querycommand) + ".", trigger.nick)
@@ -189,331 +191,4 @@ def query_detection(bot, trigger):
         if commandlist == []:
             return bot.notice("No commands match " + str(querycommand) + ".", trigger.nick)
         else:
-            return bot.notice("The following commands match " + str(querycommand) + ": " + spicemanip(bot, commandlist, 'andlist') + ".", trigger.nick)
-
-
-"""
-Array/List/String Manipulation
-"""
-
-
-# Hub
-def spicemanip(bot, inputs, outputtask, output_type='default'):
-
-    mainoutputtask, suboutputtask = None, None
-
-    # Input needs to be a list, but don't split a word into letters
-    if not inputs:
-        inputs = []
-    if not isinstance(inputs, list):
-        inputs = list(inputs.split(" "))
-        inputs = [x for x in inputs if x and x not in ['', ' ']]
-        inputs = [inputspart.strip() for inputspart in inputs]
-
-    # Create return
-    if outputtask == 'create':
-        return inputs
-
-    # Make temparray to preserve original order
-    temparray = []
-    for inputpart in inputs:
-        temparray.append(inputpart)
-    inputs = temparray
-
-    # Convert outputtask to standard
-    if outputtask in [0, 'complete']:
-        outputtask = 'string'
-    elif outputtask == 'index':
-        mainoutputtask = inputs[1]
-        suboutputtask = inputs[2]
-        inputs = inputs[0]
-    elif str(outputtask).isdigit():
-        mainoutputtask, outputtask = int(outputtask), 'number'
-    elif "^" in str(outputtask):
-        mainoutputtask = str(outputtask).split("^", 1)[0]
-        suboutputtask = str(outputtask).split("^", 1)[1]
-        outputtask = 'rangebetween'
-        if int(suboutputtask) < int(mainoutputtask):
-            mainoutputtask, suboutputtask = suboutputtask, mainoutputtask
-    elif str(outputtask).startswith("split_"):
-        mainoutputtask = str(outputtask).replace("split_", "")
-        outputtask = 'split'
-    elif str(outputtask).endswith(tuple(["!", "+", "-", "<", ">"])):
-        mainoutputtask = str(outputtask)
-        if str(outputtask).endswith("!"):
-            outputtask = 'exclude'
-        if str(outputtask).endswith("+"):
-            outputtask = 'incrange_plus'
-        if str(outputtask).endswith("-"):
-            outputtask = 'incrange_minus'
-        if str(outputtask).endswith(">"):
-            outputtask = 'excrange_plus'
-        if str(outputtask).endswith("<"):
-            outputtask = 'excrange_minus'
-        for r in (("!", ""), ("+", ""), ("-", ""), ("<", ""), (">", "")):
-            mainoutputtask = mainoutputtask.replace(*r)
-    if mainoutputtask == 'last':
-        mainoutputtask = len(inputs)
-
-    if outputtask == 'string':
-        returnvalue = inputs
-    else:
-        returnvalue = eval('spicemanip_' + outputtask + '(bot, inputs, outputtask, mainoutputtask, suboutputtask)')
-
-    # default return if not specified
-    if output_type == 'default':
-        if outputtask in [
-                            'string', 'number', 'rangebetween', 'exclude', 'random',
-                            'incrange_plus', 'incrange_minus', 'excrange_plus', 'excrange_minus'
-                            ]:
-            output_type = 'string'
-        elif outputtask in ['count']:
-            output_type = 'dict'
-
-    # verify output is correct
-    if output_type == 'return':
-        return returnvalue
-    if output_type == 'string':
-        if isinstance(returnvalue, list):
-            returnvalue = ' '.join(returnvalue)
-    elif output_type in ['list', 'array']:
-        if not isinstance(returnvalue, list):
-            returnvalue = list(returnvalue.split(" "))
-            returnvalue = [x for x in returnvalue if x and x not in ['', ' ']]
-            returnvalue = [inputspart.strip() for inputspart in returnvalue]
-    return returnvalue
-
-
-# compare 2 lists, based on the location of an index item, passthrough needs to be [indexitem, arraytoindex, arraytocompare]
-def spicemanip_index(bot, indexitem, outputtask, arraytoindex, arraytocompare):
-    item = ''
-    for x, y in zip(arraytoindex, arraytocompare):
-        if x == indexitem:
-            item = y
-    return item
-
-
-# split list by string
-def spicemanip_split(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    split_array = []
-    restring = ' '.join(inputs)
-    if mainoutputtask not in inputs:
-        split_array = [restring]
-    else:
-        split_array = restring.split(mainoutputtask)
-    split_array = [x for x in split_array if x and x not in ['', ' ']]
-    split_array = [inputspart.strip() for inputspart in split_array]
-    if split_array == []:
-        split_array = [[]]
-    return split_array
-
-
-# dedupe list
-def spicemanip_dedupe(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    newlist = []
-    for inputspart in inputs:
-        if inputspart not in newlist:
-            newlist.append(inputspart)
-    return newlist
-
-
-# Sort list
-def spicemanip_sort(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    return sorted(inputs)
-
-
-# reverse sort list
-def spicemanip_rsort(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    return sorted(inputs)[::-1]
-
-
-# count items in list, return dictionary
-def spicemanip_count(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    returndict = dict()
-    if inputs == []:
-        return returndict
-    uniqueinputitems, uniquecount = [], []
-    for inputspart in inputs:
-        if inputspart not in uniqueinputitems:
-            uniqueinputitems.append(inputspart)
-    for uniqueinputspart in uniqueinputitems:
-        count = 0
-        for ele in inputs:
-            if (ele == uniqueinputspart):
-                count += 1
-        uniquecount.append(count)
-    for inputsitem, unumber in zip(uniqueinputitems, uniquecount):
-        returndict[inputsitem] = unumber
-    return returndict
-
-
-# random item from list
-def spicemanip_random(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    randomselectlist = []
-    for temppart in inputs:
-        randomselectlist.append(temppart)
-    while len(randomselectlist) > 1:
-        random.shuffle(randomselectlist)
-        randomselect = randomselectlist[random.randint(0, len(randomselectlist) - 1)]
-        randomselectlist.remove(randomselect)
-    randomselect = randomselectlist[0]
-    return randomselect
-
-
-# remove random item from list
-def spicemanip_exrandom(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return []
-    randremove = spicemanip_random(bot, inputs, outputtask, mainoutputtask, suboutputtask)
-    inputs.remove(randremove)
-    return inputs
-
-
-# Convert list into lowercase
-def spicemanip_lower(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return [inputspart.lower() for inputspart in inputs]
-
-
-# Convert list to uppercase
-def spicemanip_upper(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return [inputspart.upper() for inputspart in inputs]
-
-
-# Convert list to uppercase
-def spicemanip_title(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return [inputspart.title() for inputspart in inputs]
-
-
-# Reverse List Order
-def spicemanip_reverse(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return []
-    return inputs[::-1]
-
-
-# comma seperated list
-def spicemanip_list(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return ', '.join(str(x) for x in inputs)
-
-
-# comma seperated list with and
-def spicemanip_andlist(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    if len(inputs) < 2:
-        return ' '.join(inputs)
-    lastentry = str("and " + str(inputs[len(inputs) - 1]))
-    del inputs[-1]
-    inputs.append(lastentry)
-    if len(inputs) == 2:
-        return ' '.join(inputs)
-    return ', '.join(str(x) for x in inputs)
-
-
-# comma seperated list with or
-def spicemanip_orlist(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    if len(inputs) < 2:
-        return ' '.join(inputs)
-    lastentry = str("or " + str(inputs[len(inputs) - 1]))
-    del inputs[-1]
-    inputs.append(lastentry)
-    if len(inputs) == 2:
-        return ' '.join(inputs)
-    return ', '.join(str(x) for x in inputs)
-
-
-# exclude number
-def spicemanip_exclude(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    del inputs[int(mainoutputtask) - 1]
-    return ' '.join(inputs)
-
-
-# Convert list to string
-def spicemanip_string(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return ' '.join(inputs)
-
-
-# Get number item from list
-def spicemanip_number(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    elif len(inputs) == 1:
-        return inputs[0]
-    elif int(mainoutputtask) > len(inputs) or int(mainoutputtask) < 0:
-        return ''
-    else:
-        return inputs[int(mainoutputtask) - 1]
-
-
-# Get Last item from list
-def spicemanip_last(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return inputs[len(inputs) - 1]
-
-
-# range between items in list
-def spicemanip_rangebetween(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    if not str(mainoutputtask).isdigit() or not str(suboutputtask).isdigit():
-        return ''
-    mainoutputtask, suboutputtask = int(mainoutputtask), int(suboutputtask)
-    if suboutputtask == mainoutputtask:
-        return spicemanip_number(bot, inputs, outputtask, mainoutputtask, suboutputtask)
-    if suboutputtask < mainoutputtask:
-        return []
-    if mainoutputtask < 0:
-        mainoutputtask = 1
-    if suboutputtask > len(inputs):
-        suboutputtask = len(inputs)
-    newlist = []
-    for i in range(mainoutputtask, suboutputtask + 1):
-        newlist.append(str(spicemanip_number(bot, inputs, outputtask, i, suboutputtask)))
-    if newlist == []:
-        return ''
-    return ' '.join(newlist)
-
-
-# Forward Range includes index number
-def spicemanip_incrange_plus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, int(mainoutputtask), len(inputs))
-
-
-# Reverse Range includes index number
-def spicemanip_incrange_minus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, 1, int(mainoutputtask))
-
-
-# Forward Range excludes index number
-def spicemanip_excrange_plus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, int(mainoutputtask) + 1, len(inputs))
-
-
-# Reverse Range excludes index number
-def spicemanip_excrange_minus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, 1, int(mainoutputtask) - 1)
+            return bot.notice("The following commands match " + str(querycommand) + ": " + spicemanip.main(bot, commandlist, 'andlist') + ".", trigger.nick)
